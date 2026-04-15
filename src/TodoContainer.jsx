d67e6806f006df027405fc28f8645cfadf7a49d7
Mohamad Bashyr <mbashirkuja@gmail.com>
2026-04-15 03:02:03 +0300

    feat: implement update and delete functionality in TaskCard component with dialog support

--- src/TodoContainer.jsx
+++ src/TodoContainer.jsx
@@ -11,33 +11,34 @@ import ToggleButton from "@mui/material/ToggleButton";
 import { v4 as uuidv4 } from "uuid";
 import { useState, useEffect, useContext, useMemo } from "react";
 import { DataContext } from "./Data";
+import { blue, red } from "@mui/material/colors";
+
+// dialog
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 
 export default function OutlinedCard() {
   const [toggleType, setToggleType] = useState("All");
-  const {Data, setData} = useContext(DataContext);
-  const [newTask, setNewTask] = useState({ 
-    title: "", 
-    description: "", 
-    isCompleted: false 
+  const { Data, setData } = useContext(DataContext);
+  const [newTask, setNewTask] = useState({
+    title: "",
+    description: "",
+    isCompleted: false,
   });
-
+  const [task, setTask] = useState(null);
 
   let filteredData = useMemo(() => {
-  if (toggleType === "Active") {
-     return Data.filter((task) => !task.isCompleted);
-  } else if (toggleType === "Completed") {
-    return Data.filter((task) => task.isCompleted);
-  } else{
-    return Data;
-  }
-}, [Data, toggleType]);
- 
-  const tasks = filteredData.map((task) => {
-    return (
-      <TaskCard key={task.id} task={task} />
-    );
-  });
-
+    if (toggleType === "Active") {
+      return Data.filter((task) => !task.isCompleted);
+    } else if (toggleType === "Completed") {
+      return Data.filter((task) => task.isCompleted);
+    } else {
+      return Data;
+    }
+  }, [Data, toggleType]);
 
   useEffect(() => {
     const storedData = localStorage.getItem("tasks");
@@ -51,86 +52,232 @@ export default function OutlinedCard() {
     if (newTask.title.trim() === "" || newTask.description.trim() === "") {
       return;
     }
-    const newData =[
+    const newData = [
       ...Data,
-      { 
+      {
         id: uuidv4(),
-        title: newTask.title, 
-        description: newTask.description, 
-        isCompleted: newTask.isCompleted 
+        title: newTask.title,
+        description: newTask.description,
+        isCompleted: newTask.isCompleted,
       },
-    ]
+    ];
     setData(newData);
     localStorage.setItem("tasks", JSON.stringify(newData));
     setNewTask({ title: "", description: "", isCompleted: false });
   };
 
-  return (
-    <Box sx={{ width: "100%" }}>
-      <Card variant="outlined" sx={{ borderRadius: "20px" , marginY: "20px" }}>
-        {
-          <>
-            <CardContent>
-              <Typography variant="h3">Tasks</Typography>
-              <hr></hr>
-              <ToggleButtonGroup
-               color="primary"
-                display={"flex"}
-                gap={1}
-                justifyContent={"center"}
-                sx={{ marginTop: "10px" }}
-                value={toggleType}
-                onChange={(e) => setToggleType(e.target.value)}
-              >
-                <ToggleButton value={"All"} variant="contained">All</ToggleButton>
-                <ToggleButton value={"Active"} variant="contained">Active</ToggleButton>
-                <ToggleButton value={"Completed"} variant="contained">Completed</ToggleButton>
-              </ToggleButtonGroup>
-            </CardContent>
-            <CardContent xs={12}>
-              {tasks}
-            </CardContent>
-            <CardContent>
-              <Grid container spacing={2} justifyContent={"center"}>
-                <Grid item xs={9}>
-                  <TextField
-                    fullWidth
-                    label="Add a title"
-                    variant="outlined"
-                    size="small"
-                    value={newTask.title}
-                    onChange={(e) => setNewTask({ ...newTask, title: e.target.value })}
-                  />
-                </Grid>
-                <Grid item xs={9}>
-                  <TextField
-                    fullWidth
-                    label="Add a description"
-                    variant="outlined"
-                    size="small"
-                    value={newTask.description}
-                    onChange={(e) => setNewTask({ ...newTask, description: e.target.value })}
-                  />
-                </Grid>
+  // 🔁 Update dialog
+  const [openUpdate, setOpenUpdate] = useState(false);
+  const [updateTask, setUpdateTask] = useState({
+    title: "",
+    description: "",
+  });
 
-                <Grid item xs={3}>
-                  <Button
-                    fullWidth
-                    variant="contained"
-                    size="large"
-                    onClick={() =>
-                      handleAddTask()
-                    }
-                    disabled={newTask.title.trim() === "" || newTask.description.trim() === ""}
-                  >
-                    Add
-                  </Button>
+  const handleClickOpenUpdate = (task) => {
+    setTask(task);
+    setUpdateTask({ title: task.title, description: task.description });
+    setOpenUpdate(true);
+  };
+
+  const handleUpdate = (task) => {
+    const updatedDataBeforeUpdate = Data.map((t) => {
+      if (t.id === task.id) {
+        return {
+          ...t,
+          title: updateTask.title,
+          description: updateTask.description,
+        };
+      }
+      return t;
+    });
+    setData(updatedDataBeforeUpdate);
+    localStorage.setItem("tasks", JSON.stringify(updatedDataBeforeUpdate));
+    setOpenUpdate(false);
+  };
+
+  // ➖ Delete dialog
+  const [openDelete, setOpenDelete] = useState(false);
+
+  const handleClickOpenDelete = (task) => {
+    setOpenDelete(true);
+    setTask(task);
+  };
+
+  const handleDelete = (task) => {
+    const updatedDataBeforeDelete = Data.filter((t) => t.id !== task.id);
+    setData(updatedDataBeforeDelete);
+    localStorage.setItem("tasks", JSON.stringify(updatedDataBeforeDelete));
+    setOpenDelete(false);
+  };
+
+  // Close both dialogs
+  const handleClose = () => {
+    setOpenDelete(false);
+    setOpenUpdate(false);
+  };
+
+  const tasks = filteredData.map((t) => {
+    return (
+      <TaskCard
+        key={t.id}
+        task={t}
+        update={handleClickOpenUpdate}
+        deleteT={handleClickOpenDelete}
+      />
+    );
+  });
+
+  return (
+    <>
+      {/* Delete dialog */}
+      <Dialog
+        open={openDelete}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle
+          id="alert-dialog-title"
+          sx={{ color: red[500], fontWeight: "bold" }}
+        >
+          {"Delete Task"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Are you sure you want to delete the task?
+            <Typography>{task?.title}</Typography>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleDelete} variant="contained" color="error">
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
+      {/* Delete dialog */}
+
+      {/* Update dialog */}
+      <Dialog
+        open={openUpdate}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle
+          id="alert-dialog-title"
+          sx={{ color: blue[500], fontWeight: "bold" }}
+        >
+          {"Update Task"}
+        </DialogTitle>
+        <DialogContent>
+          <TextField
+            fullWidth
+            label="Add a title"
+            variant="outlined"
+            size="small"
+            value={updateTask.title}
+            onChange={(e) => {
+              setUpdateTask({ ...updateTask, title: e.target.value });
+            }}
+          />
+          <TextField
+            fullWidth
+            label="Add a description"
+            variant="outlined"
+            size="small"
+            sx={{ marginTop: "10px" }}
+            value={updateTask.description}
+            onChange={(e) => {
+              setUpdateTask({ ...updateTask, description: e.target.value });
+            }}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleUpdate} variant="contained" color="primary">
+            Update
+          </Button>
+        </DialogActions>
+      </Dialog>
+      {/* Update dialog */}
+      <Box sx={{ width: "100%" }}>
+        <Card variant="outlined" sx={{ borderRadius: "20px", marginY: "20px" }}>
+          {
+            <>
+              <CardContent>
+                <Typography variant="h3">Tasks</Typography>
+                <hr></hr>
+                <ToggleButtonGroup
+                  color="primary"
+                  sx={{
+                    display: "flex",
+                    gap: 1,
+                    justifyContent: "center",
+                    marginTop: "10px",
+                  }}
+                  value={toggleType}
+                  onChange={(e) => setToggleType(e.target.value)}
+                >
+                  <ToggleButton value={"All"} variant="contained">
+                    All
+                  </ToggleButton>
+                  <ToggleButton value={"Active"} variant="contained">
+                    Active
+                  </ToggleButton>
+                  <ToggleButton value={"Completed"} variant="contained">
+                    Completed
+                  </ToggleButton>
+                </ToggleButtonGroup>
+              </CardContent>
+              <CardContent xs={12}>{tasks}</CardContent>
+              <CardContent>
+                <Grid container spacing={2} justifyContent={"center"}>
+                  <Grid item xs={9}>
+                    <TextField
+                      fullWidth
+                      label="Add a title"
+                      variant="outlined"
+                      size="small"
+                      value={newTask.title}
+                      onChange={(e) =>
+                        setNewTask({ ...newTask, title: e.target.value })
+                      }
+                    />
+                  </Grid>
+                  <Grid item xs={9}>
+                    <TextField
+                      fullWidth
+                      label="Add a description"
+                      variant="outlined"
+                      size="small"
+                      value={newTask.description}
+                      onChange={(e) =>
+                        setNewTask({ ...newTask, description: e.target.value })
+                      }
+                    />
+                  </Grid>
+
+                  <Grid item xs={3}>
+                    <Button
+                      fullWidth
+                      variant="contained"
+                      size="large"
+                      onClick={() => handleAddTask()}
+                      disabled={
+                        newTask.title.trim() === "" ||
+                        newTask.description.trim() === ""
+                      }
+                    >
+                      Add
+                    </Button>
+                  </Grid>
                 </Grid>
-              </Grid>
-            </CardContent>
-          </>
-        }
-      </Card>
-    </Box>
+              </CardContent>
+            </>
+          }
+        </Card>
+      </Box>
+    </>
   );
 }
--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,27 +1,17 @@
 import Card from "@mui/material/Card";
 import Typography from "@mui/material/Typography";
-import { blue, indigo, red } from "@mui/material/colors";
 import Grid from "@mui/material/Grid";
 import Checkbox from "@mui/material/Checkbox";
-import Button from "@mui/material/Button";
-import { useState } from "react";
-
+import { indigo, red } from "@mui/material/colors";
 // Icons
 import EditIcon from "@mui/icons-material/Edit";
 import IconButton from "@mui/material/IconButton";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { useContext } from "react";
 import { DataContext } from "../Data";
-import TextField from "@mui/material/TextField";
 
-// dialog
-import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
-import DialogTitle from "@mui/material/DialogTitle";
 
-function TaskCard({ task }) {
+function TaskCard({ task, update, deleteT }) {
   const { Data, setData } = useContext(DataContext);
 
   function clickedComplete() {
@@ -36,125 +26,8 @@ function TaskCard({ task }) {
     setData(updatedCompleted);
   }
 
-  // ➖ Delete dialog
-  const [openDelete, setOpenDelete] = useState(false);
-
-  const handleClickOpenDelete = () => {
-    setOpenDelete(true);
-  };
-
-  const handleDelete = () => {
-    const updatedDataBeforeDelete = Data.filter((t) => t.id !== task.id);
-    setData(updatedDataBeforeDelete);
-    localStorage.setItem("tasks", JSON.stringify(updatedDataBeforeDelete));
-    setOpenDelete(false);
-  };
-
-  // 🔁 Update dialog
-  const [openUpdate, setOpenUpdate] = useState(false);
-  const [updateTask, setUpdateTask] = useState({
-    title: task.title,
-    description: task.description,
-  });
-
-  const handleClickOpenUpdate = () => {
-    setOpenUpdate(true);
-  };
-
-  const handleUpdate = () => {
-    const updatedDataBeforeUpdate = Data.map((t) => {
-     if (t.id === task.id) {
-        return { ...t, title: updateTask.title, description: updateTask.description };
-      }
-      return t;
-
-    });
-    setData(updatedDataBeforeUpdate);
-    localStorage.setItem("tasks", JSON.stringify(updatedDataBeforeUpdate));
-    setOpenUpdate(false);
-  }
-
-  // Close both dialogs
-  const handleClose = () => {
-    setOpenDelete(false);
-    setOpenUpdate(false);
-  };
-
   return (
     <>
-      {/* Update dialog */}
-      <Dialog
-        open={openUpdate}
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle
-          id="alert-dialog-title"
-          sx={{ color: blue[500], fontWeight: "bold" }}
-        >
-          {"Update Task"}
-        </DialogTitle>
-        <DialogContent>
-          <TextField
-            fullWidth
-            label="Add a title"
-            variant="outlined"
-            size="small"  
-            value={updateTask.title}
-            onChange={(e) =>{
-              setUpdateTask({...updateTask, title: e.target.value})
-            }}
-          />
-          <TextField
-            fullWidth
-            label="Add a description"
-            variant="outlined"
-            size="small"  
-            sx={{ marginTop: "10px"}}
-            value={updateTask.description}
-            onChange={(e) => {
-              setUpdateTask({...updateTask, description: e.target.value})
-            }}
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleUpdate} variant="contained" color="primary">
-            Update
-          </Button>
-        </DialogActions>
-      </Dialog>
-      {/* Update dialog */}
-
-      {/* Delete dialog */}
-      <Dialog
-        open={openDelete}
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle
-          id="alert-dialog-title"
-          sx={{ color: red[500], fontWeight: "bold" }}
-        >
-          {"Delete Task"}
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            Are you sure you want to delete the task?
-            <Typography>{task.title}</Typography>
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleDelete} variant="contained" color="error">
-            Delete
-          </Button>
-        </DialogActions>
-      </Dialog>
-      {/* Delete dialog */}
-
       <Card
         variant="outlined"
         sx={{
@@ -188,19 +61,19 @@ function TaskCard({ task }) {
               <IconButton
                 aria-label="delete"
                 sx={{ color: red[500], boxShadow: 3 }}
-                onClick={handleClickOpenDelete}
+                onClick={() => deleteT(task)}
               >
                 <DeleteIcon />
               </IconButton>
               <IconButton
                 aria-label="edit"
                 sx={{ boxShadow: 3 }}
-                onClick={handleClickOpenUpdate}
+                onClick={()=> update(task)}
               >
                 <EditIcon />
               </IconButton>
               <Checkbox
-                checked={task.isCompleted}
+                checked={task.isCompleted || false}
                 sx={{ boxShadow: 3 }}
                 onClick={() => clickedComplete()}
               />
