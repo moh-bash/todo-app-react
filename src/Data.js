44ff86c5f3395f0b80673fadd267dec4decf4104
Mohamad Bashyr <mbashirkuja@gmail.com>
2026-06-22 23:43:29 +0300

    feat: implement useReducer for state management in DataContext

--- src/Data.js
+++ src/Data.js
@@ -1,2 +1,15 @@
-import { createContext } from "react";
-export const DataContext = createContext();
+import { createContext, useReducer } from "react";
+import { todoReducer } from "./reducer";
+
+const DataContext = createContext([]);
+const todoProvider =(children)=>{
+    const [Data, dispatch] = useReducer(todoReducer, []);
+    return (
+        <DataContext.Provider value={{ Data, dispatch }}>
+            {children}
+        </DataContext.Provider>
+    )
+}
+
+export { DataContext, todoProvider };
+
